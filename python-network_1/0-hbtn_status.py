4f6a2228bfa3a06bb761f46e5030b90744c4a753
Malik789321 <13734@holbertonstudents.com>
2026-06-09 12:17:27 +0000

    Task 0: fetch URL status with urllib

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Fetches a URL and displays response info using urllib
+
+from urllib import request
+
+url = "https://intranet.hbtn.io/status"
+
+with request.urlopen(url) as response:
+    body = response.read()
+
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode("utf-8")))
