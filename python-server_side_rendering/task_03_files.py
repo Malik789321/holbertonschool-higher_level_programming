ec5bc8255166846d0b509b234d189fda12064554
Malik789321 <13734@holbertonstudents.com>
2026-06-12 08:22:35 +0000

    Add product display from json and csv

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+Display products from JSON or CSV.
+"""
+
+from flask import Flask, render_template, request
+import json
+import csv
+
+app = Flask(__name__)
+
+
+def read_json():
+    with open("products.json", "r") as file:
+        return json.load(file)
+
+
+def read_csv():
+    products = []
+
+    with open("products.csv", "r") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            row["id"] = int(row["id"])
+            products.append(row)
+
+    return products
+
+
+@app.route("/products")
+def products():
+    source = request.args.get("source")
+    product_id = request.args.get("id")
+
+    if source == "json":
+        data = read_json()
+
+    elif source == "csv":
+        data = read_csv()
+
+    else:
+        return render_template(
+            "product_display.html",
+            error="Wrong source"
+        )
+
+    if product_id:
+        product_id = int(product_id)
+
+        data = [
+            product for product in data
+            if int(product["id"]) == product_id
+        ]
+
+        if not data:
+            return render_template(
+                "product_display.html",
+                error="Product not found"
+            )
+
+    return render_template(
+        "product_display.html",
+        products=data
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
